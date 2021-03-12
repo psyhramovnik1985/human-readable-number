09f4ccb33c082e67351831eb9d98171173263cad
psyhramovnik1985 <psyhramovnik@rambler.ru>
2021-03-12 15:53:41 +0300

    feat: add logics

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function toReadable (number) {
+    let mas = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    let mas2 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    let mas3 = [0, 0, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
   
-}
+    let strStart = String(number).split('');
+    let strEnd = '';
+
+    if (strStart.length === 1) {
+        strEnd += mas[Number(strStart[0])];
+        return strEnd;
+    }
+
+    if (strStart.length === 3) {
+      strEnd += mas[Number(strStart[0])]; 
+      if ((Number(strStart[strStart.length-2]) === 0 && Number(strStart[strStart.length-1]) === 0)) {
+        strEnd +=  ' hundred';
+        return strEnd;
+      } else {
+        strEnd +=  ' hundred ';
+      }
+    }    
+  
+    if (Number(strStart[strStart.length-2]) === 1) {
+      strEnd += mas2[Number(strStart[strStart.length-1])];
+      return strEnd;
+    }
+
+    if (Number(strStart[strStart.length-2]) === 0) {
+      strEnd += mas[Number(strStart[strStart.length-1])];
+      return strEnd;
+    }
+
+    if (Number(strStart[strStart.length-2]) > 1) {
+      strEnd += mas3[Number(strStart[strStart.length-2])];
+      if (Number(strStart[strStart.length-1]) === 0) {
+        return strEnd;
+      } else {
+        strEnd += ' ' + mas[Number(strStart[strStart.length-1])];
+      }    
+    }
+    return strEnd
+  }
